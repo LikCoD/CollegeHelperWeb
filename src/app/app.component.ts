3d048c63591b84f9553e79ccba0abddc707b94d0
LikDan <likdan.company@gmail.com>
2022-09-05 19:42:30 +0300

    Improve styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,5 @@
 import {Component} from '@angular/core';
-import {HttpErrorResponse} from "@angular/common/http";
-import { UserService } from './services/shared/user.service';
-import {Router} from "@angular/router";
-
-export const errorHandler = (err: HttpErrorResponse, router: Router) => {
-  if (err.error == "not authorized") {
-    console.log("login page")
-    router.navigate(["login"])
-
-    return
-  }
-
-  alert(err.error)
-}
+import {UserService} from './services/shared/user.service';
 
 @Component({
   selector: 'app-root',
