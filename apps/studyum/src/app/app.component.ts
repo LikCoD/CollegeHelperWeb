eaf5734995c52809a2987c57368adfd0ad64ba1e
LikDan <likdan.company@gmail.com>
2022-10-18 13:21:21 -0700

    Improve locale

--- apps/studyum/src/app/app.component.ts
+++ apps/studyum/src/app/app.component.ts
@@ -1,18 +1,18 @@
-import {Component} from '@angular/core';
-import {UserService} from './services/shared/user.service';
-import {TranslateService} from "@ngx-translate/core";
-import * as moment from "moment";
+import {Component} from "@angular/core"
+import {UserService} from "./services/shared/user.service"
+import {TranslateService} from "@ngx-translate/core"
+import * as moment from "moment"
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   templateUrl: "app.component.html",
   styleUrls: ["app.component.scss"]
 })
 export class AppComponent {
   constructor(public userService: UserService, private translate: TranslateService) {
-    let locale = localStorage.getItem("locale") ?? "ru"
+    let locale = localStorage.getItem("locale") ?? navigator.languages[1] ?? "en"
 
-    translate.use(locale);
+    translate.use(locale)
     moment.locale(locale)
   }
 }
