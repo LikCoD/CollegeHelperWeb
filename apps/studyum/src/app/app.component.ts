2e7d420bcb373a8d817ee219d5805f5159bea7c8
LikDan <likdan.company@gmail.com>
2022-10-18 13:28:17 -0700

    Improve locale

--- apps/studyum/src/app/app.component.ts
+++ apps/studyum/src/app/app.component.ts
@@ -12,7 +12,17 @@ export class AppComponent {
   constructor(public userService: UserService, private translate: TranslateService) {
     let locale = localStorage.getItem("locale") ?? navigator.languages[1] ?? "en"
 
-    translate.use(locale)
-    moment.locale(locale)
+    let defaultLocale = "en"
+    translate.use(locale).subscribe({
+      next: () => {
+        moment.locale(locale)
+        localStorage.setItem("locale", locale)
+      },
+      error: () => {
+        translate.use(defaultLocale)
+        moment.locale(defaultLocale)
+        localStorage.setItem("locale", defaultLocale)
+      }
+    })
   }
 }
