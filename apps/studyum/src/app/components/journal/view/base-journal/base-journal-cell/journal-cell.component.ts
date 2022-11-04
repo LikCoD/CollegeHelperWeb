bfa180df120aa0cb98c48035f1333bccfcb6a240
LikDan <likdan.company@gmail.com>
2022-11-04 18:29:06 +0300

    Improve struct

--- apps/studyum/src/app/components/journal/view/base-journal/base-journal-cell/journal-cell.component.ts
+++ apps/studyum/src/app/components/journal/view/base-journal/base-journal-cell/journal-cell.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core"
+import {Component, HostBinding, Input} from "@angular/core"
 
 @Component({
   selector: "app-base-journal-cell",
@@ -8,4 +8,8 @@ import {Component, Input} from "@angular/core"
 export class JournalCellComponent {
   @Input() entries: string[]
   @Input() color: string
+
+  @HostBinding("style.background") get bgColor() {
+    return this.entries.length ? this.color : "#29363E"
+  }
 }
