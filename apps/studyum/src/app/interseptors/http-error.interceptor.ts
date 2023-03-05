677ffad2b2f17e4b71f13fcf8a9409cf4f11632c
LikDan <likdan.company@gmail.com>
2023-03-05 22:42:34 +0300

    Add retry on failed requests

--- apps/studyum/src/app/interseptors/http-error.interceptor.ts
+++ apps/studyum/src/app/interseptors/http-error.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from "@angular/common/http"
-import {catchError, Observable, throwError} from "rxjs"
+import {catchError, Observable, retry, throwError, timer} from "rxjs"
 import {ToastService} from "../services/ui/toast.service"
 
 @Injectable()
@@ -19,6 +19,23 @@ export class HttpErrorInterceptor implements HttpInterceptor {
         if (err.status === 401) return throwError(() => err)
         this.toastService.showError(`(${err.status}) - ${err.error}`)
         return throwError(() => err)
+      }),
+      retry({
+        count: 4,
+        delay: (error: HttpErrorResponse, retryCount: number) => {
+          switch (error.status) {
+            case 408:
+            case 409:
+            case 412:
+            case 425:
+            case 429:
+            case 503:
+            case 504:
+              return timer(500 * retryCount)
+            default:
+              return throwError(() => error)
+          }
+        },
       })
     )
   }
