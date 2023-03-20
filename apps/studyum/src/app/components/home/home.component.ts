b0a7fbff416de92dcf628d1e652e88cfb9bba5a4
LikDan <likdan.company@gmail.com>
2023-03-20 23:50:27 +0300

    Move

---
+++ apps/studyum/src/app/components/home/home.component.ts
@@ -0,0 +1,42 @@
+import {Component} from "@angular/core"
+import {UserService} from "../../services/shared/user.service"
+import {User} from "../../models/user"
+import {Observable} from "rxjs"
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
+})
+export class HomeComponent {
+  cards: {title: string; url: string; route: string; permissions?: string[]}[] = [
+    {
+      title: "header.schedule",
+      url: "assets/schedule-gray.svg",
+      route: "schedule",
+      permissions: undefined,
+    },
+    {
+      title: "header.journal",
+      url: "assets/journal-gray.svg",
+      route: "journal",
+      permissions: [],
+    },
+  ]
+
+  user$: Observable<User | undefined>
+
+  constructor(public userService: UserService) {
+    this.user$ = this.userService.user$
+  }
+
+  checkPermissions(user: User | undefined, permissions?: string[]) {
+    if (permissions == undefined) return true
+    if (user == undefined) return false
+
+    for (let permission of permissions)
+      if (user?.permissions.findIndex((p) => p == permission) == -1) return false
+
+    return true
+  }
+}
