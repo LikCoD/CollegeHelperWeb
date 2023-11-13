ecd46aad94212829c4b6d21a51537286578838e5
likdan <likdan.company@gmail.com>
2023-11-13 19:03:20 +0300

    Add login | profile features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
-import {Routes} from "@angular/router";
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'schedule', loadChildren: () => import('./schedule/schedule.module').then(m => m.ScheduleModule) },
+  {
+    path: 'schedule',
+    loadChildren: () => import('./schedule/schedule.module').then(m => m.ScheduleModule),
+  },
   // { path: 'journal', loadChildren: () => import('./journal/journal.module').then(m => m.JournalModule) },
-  // { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
   // { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: "**", redirectTo: 'schedule'}
+  { path: '**', redirectTo: 'schedule' },
 ];
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'profile' },
   {
-    path: 'auth', loadChildren: () => import('@user/modules/auth/auth.module').then(a => a.AuthModule),
+    path: 'auth',
+    loadChildren: () => import('@user/modules/auth/auth.module').then(a => a.AuthModule),
   },
   {
-    path: 'profile', loadChildren: () => import('@user/modules/profile/profile.module').then(p => p.ProfileModule),
+    path: 'profile',
+    loadChildren: () => import('@user/modules/profile/profile.module').then(p => p.ProfileModule),
   },
 ];
