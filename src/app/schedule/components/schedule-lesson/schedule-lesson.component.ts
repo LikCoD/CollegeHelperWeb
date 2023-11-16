5254f0f9a382bf5442ddfa588c7d05631bf8af1c
likdan <likdan.company@gmail.com>
2023-11-16 22:41:56 +0300

    Improve html/css/ts

--- src/app/schedule/components/schedule-lesson/schedule-lesson.component.ts
+++ src/app/schedule/components/schedule-lesson/schedule-lesson.component.ts
@@ -1,24 +1,6 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  forwardRef,
-  inject,
-  Input,
-  NgZone,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  AbstractControl,
-  ControlValueAccessor,
-  FormControl,
-  FormGroup,
-  NG_VALIDATORS,
-  NG_VALUE_ACCESSOR,
-  ReactiveFormsModule,
-  ValidationErrors,
-  Validator,
-  Validators,
-} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ScheduleLesson } from '@schedule/entities/schedule';
 import { Router, RouterLink } from '@angular/router';
 import { Head3Component } from '@ui/text/head3.component';
@@ -38,21 +20,9 @@ import { Head4Component } from '@ui/text/head4.component';
   ],
   templateUrl: './schedule-lesson.component.html',
   styleUrls: ['./schedule-lesson.component.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => ScheduleLessonComponent),
-      multi: true,
-    },
-    {
-      provide: NG_VALIDATORS,
-      useExisting: forwardRef(() => ScheduleLessonComponent),
-      multi: true,
-    },
-  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScheduleLessonComponent implements Validator, ControlValueAccessor {
+export class ScheduleLessonComponent {
   isUpdated!: boolean;
 
   @Input() editable: boolean = false;
@@ -60,72 +30,32 @@ export class ScheduleLessonComponent implements Validator, ControlValueAccessor 
   @Input() routing: boolean = false;
   @Input() preview: boolean = false;
 
-  form = new FormGroup({
-    id: new FormControl('', Validators.required),
-    subject: new FormControl('', Validators.required),
-    teacher: new FormControl('', Validators.required),
-    room: new FormControl('', Validators.required),
-    group: new FormControl('', Validators.required),
-    primaryColor: new FormControl('#FFFFFF'),
-    secondaryColor: new FormControl('transparent'),
-  });
-
   query = {
     studyPlaceID: '',
   };
 
-  private studyPlaceID!: string;
+  lesson!: ScheduleLesson;
 
+  private studyPlaceID!: string;
   private zone = inject(NgZone);
   private router = inject(Router);
 
-  @Input()
-  set lesson(value: ScheduleLesson) {
+  @Input({ required: true, alias: 'lesson' })
+  set _lesson(value: ScheduleLesson) {
     this.isUpdated = value.isGeneral ?? false;
     this.studyPlaceID = value.studyPlaceID ?? '';
     this.query = {
       studyPlaceID: this.studyPlaceID,
     };
-    this.setFormValue(value);
-  }
-
-  validate(control: AbstractControl): ValidationErrors | null {
-    if (this.form.get('subject')?.errors != null) return this.form.get('subject')!!.errors;
-    if (this.form.get('teacher')?.errors != null) return this.form.get('teacher')!!.errors;
-    if (this.form.get('room')?.errors != null) return this.form.get('room')!!.errors;
-    if (this.form.get('group')?.errors != null) return this.form.get('group')!!.errors;
-
-    return null;
-  }
-
-  registerOnChange(fn: any): void {
-    this.form.valueChanges.subscribe(fn);
-  }
-
-  registerOnTouched(fn: any): void {}
-
-  writeValue(lesson: ScheduleLesson): void {
-    this.setFormValue(lesson);
+    this.lesson = value;
   }
 
   buildRouterLink(type: string): string {
-    return `/schedule/${type}/${this.form.get(type)?.value}`;
+    const lesson: { [key: string]: any } = this.lesson;
+    return `/schedule/${type}/${lesson[type]}`;
   }
 
   navigate(url: string): void {
     this.zone.run(() => this.router.navigate([url], { queryParams: this.query })).then();
-  }
-
-  private setFormValue(lesson: ScheduleLesson) {
-    const value = {
-      id: lesson.id || null,
-      subject: lesson.subject,
-      teacher: lesson.teacher,
-      room: lesson.room,
-      group: lesson.group,
-      primaryColor: lesson.primaryColor,
-      secondaryColor: lesson.secondaryColor || null,
-    };
-    this.form.setValue(value);
   }
 }
