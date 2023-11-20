918a5a9b6afd649894e82c3fb69831d6ed19398e
likdan <likdan.company@gmail.com>
2023-11-21 02:27:00 +0300

    Move to standalone component bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [HeaderComponent, RouterOutlet],
+  standalone: true,
 })
-export class AppComponent {
-}
+export class AppComponent {}
